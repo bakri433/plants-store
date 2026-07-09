35a31ac1d4122bf852ed76cdf8b9dae9a21e656d
bakri433 <0949109246b@gmail.com>
2026-07-09 15:04:04 +0300

    the main page completed

---
+++ script.js
@@ -0,0 +1,6 @@
+//<i class="fa-brands fa-envira" style="color: rgb(99, 230, 190);"></i>
+// .hero-section {
+//   background: linear-gradient(to right, #2d4a3e 40%, transparent 100%), 
+//               url('image-link.jpg') no-repeat center center;
+//   background-size: cover;
+// }
