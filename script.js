0f5dc9f4400b5342595a78e587214932490cf229
bakri433 <0949109246b@gmail.com>
2026-07-13 17:28:12 +0300

    classification button

--- script.js
+++ script.js
@@ -4,3 +4,26 @@
 //               url('image-link.jpg') no-repeat center center;
 //   background-size: cover;
 // }
+ let classifications = document.querySelector(".arrow");
+let arrow= document.getElementById("arrow");
+    let classlist=document.createElement("div");
+    classlist.className="classlist";
+    classifications.appendChild(classlist);
+    classlist.classList.remove("classlist");
+    classlist.style.display="none";
+
+
+// classifications.addEventListener("mouseenter",()=>{
+//    classlist.style.display="block";
+
+
+// });
+// classifications.addEventListener("mouseleave",()=>{
+//    classlist.style.display="none";
+
+
+//});
+classifications.addEventListener("click",()=>{
+    arrow.style.transform="rotateX(180deg)"
+    classlist.classList.toggle("classlist");
+})
